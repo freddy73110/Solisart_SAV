0d6a76b154f39e3b813b0b1392da7a9c559c583c
freddy73110 <64645565+freddy73110@users.noreply.github.com>
2023-11-28 17:11:36 +0100

    peaufinage

--- sav/scrapping.py
+++ sav/scrapping.py
@@ -67,7 +67,7 @@ class scrappingMySolisart():
                 import subprocess
                 if subprocess.call("killall -9 firefox", shell=True) > 0:
                     send_channel_message('cartcreating', {
-                        'message': "Firefox à l'arrêt"
+                        'message': "Arrêt de Firefox"
                     })
                 else:
                     send_channel_message('cartcreating', {
@@ -260,110 +260,111 @@ class scrappingMySolisart():
         :param dict_schematic: json schémateque converti en dict python
         :return:
         """
-        self.driver.get('https://my.solisart.fr/admin/index.php?page=installation&id='+installation)
-        i = 0
-        send_channel_message('cartcreating', {'message':'Attente '+ installation +' que la carte se connecte'})
-        while self.driver.find_element(By.ID, 'comm-statut').get_attribute(
-                "src") != 'https://my.solisart.fr/admin/image/bullet_green.png' and i < 1200:
-            time.sleep(0.5)
-            print("wait again")
-            i += 1
-        if i == 1200:
-            send_channel_message('cartcreating', {'message':"<i class='fas fa-times' style='color: #fe0101;'></i> La carte "+installation+" ne s'est pas connecté en 10 min"})
-            self.close()
-        else:
-            send_channel_message('cartcreating', {'message':'La carte ' + installation + ' est connectée'})
-            assert self.driver.find_element(By.XPATH, '//label[@for="input-pages-info"]')
-            time.sleep(2)
-            self.driver.find_element(By.XPATH, '//label[@for="input-pages-info"]').click()
-            time.sleep(2)
-            self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-suivi"]').click()
-            self.driver.find_element(By.ID, 'mail-defaut-adresses-solisart').send_keys("sav@solisart.fr")
-            print(dict_schematic)
-            if 'formulaire' in dict_schematic:
-                dict_schematictemp = dict_schematic['formulaire']
+        try:
+            self.driver.get('https://my.solisart.fr/admin/index.php?page=installation&id='+installation)
+            i = 0
+            send_channel_message('cartcreating', {'message':'Attente '+ installation +' que la carte se connecte'})
+            while self.driver.find_element(By.ID, 'comm-statut').get_attribute(
+                    "src") != 'https://my.solisart.fr/admin/image/bullet_green.png' and i < 1200:
+                time.sleep(0.5)
+                print("wait again")
+                i += 1
+            if i == 1200:
+                send_channel_message('cartcreating', {'message':"<i class='fas fa-times' style='color: #fe0101;'></i> La carte "+installation+" ne s'est pas connecté en 10 min"})
+                self.close()
             else:
-                dict_schematictemp = dict_schematic
-            if "adresse_mail" in dict_schematictemp:
-                self.driver.find_element(By.ID, 'mail-defaut-adresses-installateur').\
-                    clear()
-                print(dict_schematictemp['adresse_mail'])
-                self.driver.find_element(By.ID, 'mail-defaut-adresses-installateur'). \
-                    send_keys(dict_schematictemp['adresse_mail'])
-            if "installateur" in dict_schematictemp:
-                self.driver.find_element(By.ID, 'input-suivi-installateur'). \
-                    clear()
-                self.driver.find_element(By.ID, 'input-suivi-installateur').\
-                    send_keys(dict_schematictemp['installateur'])
-
-            self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-client"]').click()
-            for key, value in {
-                "prenom_client": 'input-client-prenom',
-                "nom_client": 'input-client-nom',
-                "mail_client": 'input-client-email',
-                "tel_client": 'input-client-telephone1'
-            }.items():
-                if key in dict_schematictemp:
-                    self.driver.find_element(By.ID, value). \
-                    clear()
-                    self.driver.find_element(By.ID, value).\
-                    send_keys(dict_schematictemp[key])
-
-            self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-localisation"]').click()
-            for key, value in {
-                "adresse_client":'input-adresse-voie1',
-                "code_postale_client": 'input-adresse-codepostal',
-                "ville_client": 'input-adresse-commune'
-            }.items():
-                if key in dict_schematictemp:
-                    self.driver.find_element(By.ID, value). \
+                send_channel_message('cartcreating', {'message':'La carte ' + installation + ' est connectée'})
+                assert self.driver.find_element(By.XPATH, '//label[@for="input-pages-info"]')
+                time.sleep(2)
+                self.driver.find_element(By.XPATH, '//label[@for="input-pages-info"]').click()
+                time.sleep(2)
+                self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-suivi"]').click()
+                self.driver.find_element(By.ID, 'mail-defaut-adresses-solisart').send_keys("sav@solisart.fr")
+                if 'formulaire' in dict_schematic:
+                    dict_schematictemp = dict_schematic['formulaire']
+                else:
+                    dict_schematictemp = dict_schematic
+                if "adresse_mail" in dict_schematictemp:
+                    self.driver.find_element(By.ID, 'mail-defaut-adresses-installateur').\
                         clear()
-                    self.driver.find_element(By.ID, value). \
+                    print(dict_schematictemp['adresse_mail'])
+                    self.driver.find_element(By.ID, 'mail-defaut-adresses-installateur'). \
+                        send_keys(dict_schematictemp['adresse_mail'])
+                if "installateur" in dict_schematictemp:
+                    self.driver.find_element(By.ID, 'input-suivi-installateur'). \
+                        clear()
+                    self.driver.find_element(By.ID, 'input-suivi-installateur').\
+                        send_keys(dict_schematictemp['installateur'])
+
+                self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-client"]').click()
+                for key, value in {
+                    "prenom_client": 'input-client-prenom',
+                    "nom_client": 'input-client-nom',
+                    "mail_client": 'input-client-email',
+                    "tel_client": 'input-client-telephone1'
+                }.items():
+                    if key in dict_schematictemp:
+                        self.driver.find_element(By.ID, value). \
+                        clear()
+                        self.driver.find_element(By.ID, value).\
                         send_keys(dict_schematictemp[key])
 
-            try:
+                self.driver.find_element(By.XPATH, '//a[@href="#onglet-info-localisation"]').click()
+                for key, value in {
+                    "adresse_client":'input-adresse-voie1',
+                    "code_postale_client": 'input-adresse-codepostal',
+                    "ville_client": 'input-adresse-commune'
+                }.items():
+                    if key in dict_schematictemp:
+                        self.driver.find_element(By.ID, value). \
+                            clear()
+                        self.driver.find_element(By.ID, value). \
+                            send_keys(dict_schematictemp[key])
 
-                url = 'https://api-adresse.data.gouv.fr/search/?q=' + dict_schematictemp['adresse_client'] +'&postcode='+ dict_schematictemp["code_postale_client"] + '&type=street'
-                import requests
-                resp = requests.get(url).json()
-                print(resp['features'][0]['geometry']['coordinates'])
-                coordonnee=resp['features'][0]['geometry']['coordinates']
-                GPS=str(coordonnee[1])+ ','+str(coordonnee[0])
+                try:
 
-                # url = 'https://nominatim.openstreetmap.org/?q=France'
-                # for at in ["adresse_client", "code_postale_client", "ville_client"]:
-                #     if at in dict_schematictemp:
-                #         url += '+' + dict_schematictemp[at].replace(' ', '%').replace('\'', '%').lower()
-                # url += '&format=json'
-                # import requests
-                # resp = requests.get(url).json()
-                # if not resp or not 'lon' in resp[0]:
-                #     GPS = None
-                #     pass
-                # else:
-                #     GPS = resp[0]['lat'] + ',' + resp[0]['lon']
+                    url = 'https://api-adresse.data.gouv.fr/search/?q=' + dict_schematictemp['adresse_client'] +'&postcode='+ dict_schematictemp["code_postale_client"] + '&type=street'
+                    import requests
+                    resp = requests.get(url).json()
+                    coordonnee=resp['features'][0]['geometry']['coordinates']
+                    GPS=str(coordonnee[1])+ ','+str(coordonnee[0])
 
-                if GPS == '46.603354,1.8883335':
-                    GPS = None
-                if GPS:
-                    self.driver.find_element(By.ID, 'input-adresse-gps'). \
-                        send_keys(GPS)
+                    # url = 'https://nominatim.openstreetmap.org/?q=France'
+                    # for at in ["adresse_client", "code_postale_client", "ville_client"]:
+                    #     if at in dict_schematictemp:
+                    #         url += '+' + dict_schematictemp[at].replace(' ', '%').replace('\'', '%').lower()
+                    # url += '&format=json'
+                    # import requests
+                    # resp = requests.get(url).json()
+                    # if not resp or not 'lon' in resp[0]:
+                    #     GPS = None
+                    #     pass
+                    # else:
+                    #     GPS = resp[0]['lat'] + ',' + resp[0]['lon']
 
-            except Exception as ex:
+                    if GPS == '46.603354,1.8883335':
+                        GPS = None
+                    if GPS:
+                        self.driver.find_element(By.ID, 'input-adresse-gps'). \
+                            send_keys(GPS)
 
-                exc_type, exc_obj, exc_tb = sys.exc_info()
+                except Exception as ex:
+                    exc_type, exc_obj, exc_tb = sys.exc_info()
+                    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                    print(exc_type, fname, exc_tb.tb_lineno)
+                    print(ex)
+                    send_channel_message('cartcreating', {'message':'Les informations GPS pour' + installation + ' ne sont pas enregistrées'})
 
-                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            self.driver.find_element(By.XPATH, '//a[@href="#onglet-enregistrement"]').click()
+            self.driver.find_element(By.ID, 'button-enregistrement-enregistrer').click()
+            send_channel_message(
+                'cartcreating',{'message':"<i class='fas fa-check' style='color: #018303;'></i> Les informations pour " + installation + " sont bien enregistrées."}
+            )
+        except Exception as ex:
+            send_channel_message('cartcreating', {'message':"Erreur: " + str(exc_type) + str(fname)+ "ligne:" + str(exc_tb.tb_lineno) + str(ex) })
+            send_channel_message('cartcreating', {
+                'message': "<i class='fas fa-times' style='color: #fe0101;'></i> Les informations sur l'installation et le propriétaire n'ont pas pu être enregistré."})
 
-                print(exc_type, fname, exc_tb.tb_lineno)
-
-                print(ex)
-                send_channel_message('cartcreating', {'message':'Les informations GPS pour' + installation + ' ne sont pas enregistrées'})
-        self.driver.find_element(By.XPATH, '//a[@href="#onglet-enregistrement"]').click()
-        self.driver.find_element(By.ID, 'button-enregistrement-enregistrer').click()
-        send_channel_message(
-            'cartcreating',{'message':"<i class='fas fa-check' style='color: #018303;'></i> Les informations pour " + installation + " sont bien enregistrées."}
-        )
 
     def cart_created_since_json(self, kwargs):
         self.json_installation = True
@@ -626,7 +627,7 @@ class scrappingMySolisart():
             send_channel_message('cartcreating',
                                  {'message': "Erreur: " + str(exc_type) + str(fname) + str(exc_tb.tb_lineno) + str(ex)})
             send_channel_message('cartcreating', {
-                'message': "<i class='fas fa-times' style='color: #fe0101;'></i> Le propriétaire n'a pas pu être affecté."})
+                'message': "<i class='fas fa-times' style='color: #fe0101;'></i> "+ idsa +" n'a pas pu être affecté."})
             self.close()
 
     def record_png(self, installation):
@@ -731,7 +732,6 @@ class scrappingMySolisart():
 
     def update_regul(self, installation):
         try:
-
             self.driver.get('https://my.solisart.fr/admin/index.php?page=installation&id=' + installation)
             send_channel_message('cartcreating', {'message':'Attente que la carte '+ installation +' se connecte'})
             self.waitelement(By.XPATH, '// *[@id="comm-statut"][@src="image/bullet_green.png"]', 'presence_of_element_located', '', time_max=1200)
@@ -761,7 +761,6 @@ class scrappingMySolisart():
             self.waitelement(By.XPATH,'//*[@id="firmware-maj"]/table/tbody/tr[2]/td[2]/img[@alt="Terminée avec succès"]',
                                  'presence_of_element_located', '', time_max=1200)
 
-            print("La carte de régulation est à la dernière version")
             send_channel_message('cartcreating',
                                      {'message':"<i class='fas fa-check' style='color: #018303;'></i> La carte de régulation de l'installation " + installation + ' est à la dernière version'})
         except Exception as ex:
@@ -866,48 +865,61 @@ class scrappingMySolisart():
         self.cart_created_since_csv_config(os.path.join(os.path.dirname(__file__), 'temp', 'config.csv'))
 
     def createuser(self, dict_schematic):
-
-        if 'formulaire' in dict_schematic:
-            dict_schematic=dict_schematic['formulaire']
-        if 'mail_client' in dict_schematic:
-            self.driver.get('https://my.solisart.fr/admin/index.php?page=creer_utilisateur')
-            if not dict_schematic['prenom_client']:
-                dict_schematic['prenom_client'] = '-'
-            for key, value in {
-                "prenom_client": 'prenom',
-                "nom_client": 'nom',
-                "mail_client": 'email',
-                "tel_client": 'telephone1',
-                "adresse_client": 'voie1',
-                "code_postale_client": 'codepostal',
-                "ville_client": 'commune',
-                "mail_client": 'id',
-            }.items():
-                if key in dict_schematic:
-                    self.driver.find_element(By.XPATH, '//input[@name="'+ value +'"]'). \
-                        clear()
-                    self.driver.find_element(By.XPATH, '//input[@name="'+ value +'"]'). \
-                        send_keys(dict_schematic[key])
-            self.driver.find_element(By.XPATH, '//input[@name="pass_saisie"]'). \
-                clear()
-            self.driver.find_element(By.XPATH, '//input[@name="pass_saisie"]'). \
-                send_keys('solaire')
-            self.driver.find_element(By.XPATH, '//input[@name="pass_confirm"]'). \
-                clear()
-            self.driver.find_element(By.XPATH, '//input[@name="pass_confirm"]'). \
-                send_keys('solaire')
-            self.driver.find_element(By.XPATH, '//input[@name="creation"]'). \
-                click()
-            send_channel_message('cartcreating', {'message':"<i class='fas fa-check' style='color: #018303;'></i> Le propriétaire "+ \
-                dict_schematic['prenom_client']+ \
-                " " + \
-                dict_schematic['nom_client'] + \
-                " a correctement été enregistré."})
-            return True
-        else:
+        try:
+            if 'formulaire' in dict_schematic:
+                dict_schematic=dict_schematic['formulaire']
+            if 'mail_client' in dict_schematic:
+                self.driver.get('https://my.solisart.fr/admin/index.php?page=creer_utilisateur')
+                if not dict_schematic['prenom_client']:
+                    dict_schematic['prenom_client'] = '-'
+                for key, value in {
+                    "prenom_client": 'prenom',
+                    "nom_client": 'nom',
+                    "mail_client": 'email',
+                    "tel_client": 'telephone1',
+                    "adresse_client": 'voie1',
+                    "code_postale_client": 'codepostal',
+                    "ville_client": 'commune',
+                    "mail_client": 'id',
+                }.items():
+                    if key in dict_schematic:
+                        self.driver.find_element(By.XPATH, '//input[@name="'+ value +'"]'). \
+                            clear()
+                        self.driver.find_element(By.XPATH, '//input[@name="'+ value +'"]'). \
+                            send_keys(dict_schematic[key])
+                self.driver.find_element(By.XPATH, '//input[@name="pass_saisie"]'). \
+                    clear()
+                self.driver.find_element(By.XPATH, '//input[@name="pass_saisie"]'). \
+                    send_keys('solaire')
+                self.driver.find_element(By.XPATH, '//input[@name="pass_confirm"]'). \
+                    clear()
+                self.driver.find_element(By.XPATH, '//input[@name="pass_confirm"]'). \
+                    send_keys('solaire')
+                self.driver.find_element(By.XPATH, '//input[@name="creation"]'). \
+                    click()
+                send_channel_message('cartcreating', {'message':"<i class='fas fa-check' style='color: #018303;'></i> Le propriétaire "+ \
+                    dict_schematic['prenom_client']+ \
+                    " " + \
+                    dict_schematic['nom_client'] + \
+                    " a correctement été enregistré."})
+                return True
+            else:
+                send_channel_message(
+                    'cartcreating',
+                    {'message':"<i class='fas fa-times' style='color: #fe0101;'></i> Le propriétaire n'a pas pu être créé."}
+                )
+                return False
+        except Exception as ex:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            print(exc_type, fname, exc_tb.tb_lineno)
+            print(ex)
+            send_channel_message('cartcreating', {
+                'message': "<i class='fas fa-times' style='color: #fe0101;'></i> Erreur: " + str(exc_type) + str(
+                    fname) + str(exc_tb.tb_lineno) + str(ex)})
             send_channel_message(
-                'cartcreating',
-                {'message':"<i class='fas fa-times' style='color: #fe0101;'></i> Le propriétaire n'a pas pu être créé."}
-            )
-            return False
+                    'cartcreating',
+                    {'message':"<i class='fas fa-times' style='color: #fe0101;'></i> Le propriétaire n'a pas pu être créé."}
+                )
+                return False
 
