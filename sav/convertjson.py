0e06b226ed86dfa7ac2e145b7d924f2fd5f2fbc0
freddy73110 <64645565+freddy73110@users.noreply.github.com>
2024-10-28 14:23:47 +0100

    BUG: Scrapping with Geoinfo return

--- sav/convertjson.py
+++ sav/convertjson.py
@@ -125,7 +125,7 @@ class convertjson:
             "Configuration du " + timezone.now().strftime("%d/%m/%Y à %H:%M"),
         ]
         if path:
-            df.to_csv(path, sep=";", header=True, index=False, columns=None)
+            df.to_csv(path, sep=";", header=True, index=False, columns=None), msg
         else:
             from django.http import HttpResponse
 
